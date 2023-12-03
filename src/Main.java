2a53a4c504374a64e77b6dd72698d663e3c21cba
kaomiwq <ananyin.vova2019@mail.ru>
2023-12-03 14:46:50 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Random random = new Random();
+
+        Scanner scanner = new Scanner(System.in);
+
+        int count  = 0;
+        System.out.print("Введите колличество чисел: ");
+        count = scanner.nextInt();
+
+        int[] numb = new int[count];
+        for (int i = 0; i < numb.length; i++) {
+            System.out.print(String.format("Значение %d числа : ", i + 1, count));
+            numb[i] = random.nextInt(10 - 5 + 1 ) + 5;
+            System.out.println(numb[i]);
+        }
+        System.out.print("Введите значение A: ");
+        int a = scanner.nextInt();
+        for (int i = 0; i < numb.length; i++) {
+            System.out.print(String.format("Значение %d числа уменьшенное на число A:  ", i + 1, count));
+            numb[i] = numb[i] - a;
+            System.out.println(numb[i]);
+        }
+    }
+}
